2022760ffe14fc8dac5993a6dae147d3fcc874c5
Krzysztof Bera <krzysztof.bera9@gmail.com>
2021-12-05 13:14:35 +0100

    unit testing

--- run_tb.py
+++ run_tb.py
@@ -1,8 +1,8 @@
-from sawtooth_signal_tb import sawtooth_tb, results
+from sawtooth_signal_tb import sawtooth_tb
 from system_settings import SystemSettings
 
 
-system_settings = SystemSettings()
+system_settings = SystemSettings(desired_clk_freq=2e6, bit_width=16, desired_wave_freq=1000)
 tb = sawtooth_tb(system_settings, periods=8)
-# tb.config_sim(trace=True)
+tb.config_sim(trace=True)
 tb.run_sim()
---
+++ unit_testing.py
@@ -0,0 +1,50 @@
+import unittest
+from system_settings import SystemSettings
+from sawtooth_signal_tb import sawtooth_tb
+import sawtooth_signal_tb
+
+
+class TestSawtoothSignalGenerator(unittest.TestCase):
+    """
+    A class for testing of the block generating sawtooth signal
+    """
+
+    def test_periods_signal(self):
+        """
+        Simple test case with clock frequency 0.5 MHz, 16 bit width, desired period of signal 350 ns.
+        Generating from 1 to 7 periods of the signal
+        """
+        self.system_settings = SystemSettings()
+
+        for period_to_run in range(1, 8):
+            self.periods_to_run = period_to_run
+            tb = sawtooth_tb(self.system_settings, periods=self.periods_to_run)
+            tb.run_sim()
+
+            expected_wave = self.get_expected_wave()
+            self.assertEqual(sawtooth_signal_tb.results, expected_wave)
+            sawtooth_signal_tb.results = []
+
+    def test_signal_frequency(self):
+        """
+        Testing frequency of the generated signal. Frequency 200 MHz, 16 bit width, frequency of the generated wave
+        varies. Generating 8 periods of the signal
+        """
+        for signal_freq in (1000, 2000, 3000, 4000, 5000):
+            self.periods_to_run = 8
+            self.system_settings = SystemSettings(desired_clk_freq=2e6, bit_width=16, desired_wave_freq=signal_freq)
+            tb = sawtooth_tb(self.system_settings, periods=self.periods_to_run)
+            tb.run_sim()
+
+            expected_wave = self.get_expected_wave()
+            self.assertEqual(sawtooth_signal_tb.results, expected_wave)
+            sawtooth_signal_tb.results = []
+
+    def get_expected_wave(self):
+        return [num % self.system_settings.phase_limit for num in range(0, self.periods_to_run *
+                                                                        self.system_settings.phase_limit)]
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+
