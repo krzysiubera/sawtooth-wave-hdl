46663264290acb978714fca46e90f4b8a5153671
Krzysztof Bera <krzysztof.bera9@gmail.com>
2021-12-07 22:16:31 +0100

    upload image

--- run_tb.py
+++ run_tb.py
@@ -2,7 +2,7 @@ from sawtooth_signal_tb import sawtooth_tb
 from system_settings import SystemSettings
 
 
-system_settings = SystemSettings(desired_clk_freq=0.5*1e9, bit_width=16, desired_wave_freq=1/350e-7)
-tb = sawtooth_tb(system_settings, periods=8)
+system_settings = SystemSettings(desired_clk_freq=0.5*1e9, bit_width=16, desired_wave_freq=1/1000e-9)
+tb = sawtooth_tb(system_settings, periods=4)
 tb.config_sim(trace=True)
 tb.run_sim()
